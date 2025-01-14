86babc0db507c3846537a24ed83165f260e0e079
Willian Alexander Franco Reyes <willianalexanderfrancoreyes@MacBook-Air-de-Willian.local>
2025-01-14 11:18:07 -0500

    add first commit, base project

---
+++ applications/app-service/src/main/java/co/com/companywf/config/UseCasesConfig.java
@@ -0,0 +1,14 @@
+package co.com.companywf.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(basePackages = "co.com.companywf.usecase",
+        includeFilters = {
+                @ComponentScan.Filter(type = FilterType.REGEX, pattern = "^.+UseCase$")
+        },
+        useDefaultFilters = false)
+public class UseCasesConfig {
+}
